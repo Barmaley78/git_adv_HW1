c1edad5ec4e6a2169c7d065a57dcefac5848c26a
Sergey <barmaley78@mail.ru>
2023-12-11 17:49:56 +0700

    script file is commented

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
 const images = JSON.parse(dataInfo);
+
+// finding div element with class name content
 let cont = document.querySelector('.content');
 
+// adding to div element each object from data file and formatting it 
 for (const [key, value] of Object.entries(images)) {
 
     const itemEl = document.createElement('div');
